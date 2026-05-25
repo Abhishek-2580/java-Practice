e2dac4f5e7da5575e03c162ff66cdae0911f9783
Abhishek Kumar Sah <sah571394@gmail.com>
2026-05-25 21:47:32 +0530

    practice

---
+++ Garland.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Garland {
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n=sc.nextInt();
+        while(n>0){
+            System.out.println("Enter the red:");
+            int R=sc.nextInt();
+            System.out.println("Enter the green:");
+            int G=sc.nextInt();
+            System.out.println("Enter the blue:");
+            int B=sc.nextInt();
+            int max=Math.max(R,Math.max(G,B));
+            int sum=R+G+B;
+            if(max<=(sum-max)+1){
+                System.out.println("Yes");
+            }
+            else{
+                System.out.println("NO");
+            }
+            n--;
+        }
+        sc.close();
+    }
+}
---
+++ String.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class String {
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        String str =sc.next();
+        String str1=sc.next();
+        str=str.compareTo(str1)
+        if(str<str1){
+
+        }
+
+
+    }
+}
